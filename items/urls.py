ca1248cfc30f0dfddca689b6c962e9bf151e0a03
suidonx <neesama178@gmail.com>
2025-09-16 12:11:19 +0900

    商品詳細機能の追加

--- items/urls.py
+++ items/urls.py
@@ -6,4 +6,5 @@ app_name = "items"
 
 urlpatterns = [
     path("", views.Index.as_view(), name="index"),
+    path("<int:pk>/", views.Detail.as_view(), name="detail"),
 ]
--- items/views.py
+++ items/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 
 from .models import Item
 
@@ -8,3 +9,13 @@ from .models import Item
 class Index(ListView):
     template_name = "items/index.html"
     model = Item
+
+
+class Detail(DetailView):
+    template_name = "items/detail.html"
+    model = Item
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["latest_product"] = Item.objects.latest("id")
+        return context
