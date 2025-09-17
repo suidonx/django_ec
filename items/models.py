45c2409831b85d2653cba1465f4c1d413d2cfaea
suidonx <neesama178@gmail.com>
2025-09-17 22:31:12 +0900

    modelのpriceフィールドにチェック制約を追加

--- items/models.py
+++ items/models.py
@@ -10,3 +10,8 @@ class Item(models.Model):
     image = models.ImageField(upload_to="images/")
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        constraints = [
+            models.CheckConstraint(check=models.Q(price__gt=0), name="price_gt_0"),
+        ]
