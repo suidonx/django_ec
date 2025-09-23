f7d3a9065ad7d2e49cfe344bd12495df50fe8648
suidonx <neesama178@gmail.com>
2025-09-24 02:09:43 +0900

    管理者用の商品一覧ページを作成

--- manage_items/views.py
+++ manage_items/views.py
@@ -1,10 +1,33 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
 
-from .models import Item
+from items.models import Item
 
 
 # Create your views here.
 class Index(ListView):
     template_name = "manage_items/index.html"
     model = Item
+
+    # コンテキストを追加
+    def get_context_data(self, **kwargs):
+        # フィールド名の一覧を取得
+        fields = Item._meta.get_fields()
+        fields_names = [v.name for v in fields]
+
+        # 各レコードの属性値を取得
+        items = Item.objects.all()
+        attrs = []
+        for item in items:
+            attr = []
+            for field in fields_names:
+                attr.append(getattr(item, field))
+            attrs.append(attr)
+
+        context = super().get_context_data(**kwargs)
+        extra_context = {
+            "fields_names": fields_names,
+            "attrs": attrs,
+        }
+        context.update(extra_context)
+        return context
