ba93612dc92d1bba122f355f07c5a2f289114d33
suidonx <neesama178@gmail.com>
2025-09-24 01:52:12 +0900

    ruffの適用

--- manage_items/apps.py
+++ manage_items/apps.py
@@ -2,5 +2,5 @@ from django.apps import AppConfig
 
 
 class ManageItemsConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'manage_items'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "manage_items"
