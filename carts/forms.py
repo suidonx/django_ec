778ab1ce69b8f97ea406b0b73dd56f404b3f8e48
suidonx <neesama178@gmail.com>
2025-09-30 19:51:57 +0900

    Formで空欄のまま送信できないように修正、他、冗長な部分を削除

--- carts/forms.py
+++ carts/forms.py
@@ -5,7 +5,6 @@ class AddToCartForm(forms.Form):
     quantity = forms.IntegerField(
         min_value=1,
         initial=1,
-        required=False,
         label="",
-        widget=forms.NumberInput(attrs={"class": "form-control", "value": 1}),
+        widget=forms.NumberInput(attrs={"class": "form-control"}),
     )
